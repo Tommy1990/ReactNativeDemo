dc735ce2cddc42557b772a64e603a223377bb85c
Tommy1990 <majipeng1990@163.com>
2019-04-19 17:59:17 +0800

    video

--- SRC/Home/ParkVideoDetailPage.js
+++ SRC/Home/ParkVideoDetailPage.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react';
 import {SafeAreaView,View,Button,Text,TouchableOpacity,Image} from 'react-native'
-import {video} from 'react-native-video';
+import Video from 'react-native-video';
+import commenStyles from '../Base/CommenStyle';
 export default class ParkVideoDetailPage extends Component{
     static navigationOptions = ({navigation})=>{
        let model = navigation.getParam('model','');
@@ -11,8 +12,16 @@ export default class ParkVideoDetailPage extends Component{
         }
     }
     render(){
-        return (<SafeAreaView>
+        let model = this.props.navigation.getParam('model',null);
+        var videoSource = {uri:''};
+        if (model != null){
+             let url = model.nf_liveAddress.replace('rtmp://rtmp','http://hls') + '.m3u8';
             
+            videoSource = {uri:url};
+            
+        }
+        return (<SafeAreaView style={commenStyles.containerNormal}>
+            <Video source={videoSource} style={{width:'100%',height:300}}></Video>
         </SafeAreaView>)
     }
 }
--- android/app/src/main/java/com/mydemoapp/MainApplication.java
+++ android/app/src/main/java/com/mydemoapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.mydemoapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.brentvatne.react.ReactVideoPackage;
 import ca.jaysoo.extradimensions.ExtraDimensionsPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactVideoPackage(),
             new ExtraDimensionsPackage(),
             new RNDeviceInfo(),
             new AsyncStoragePackage(),
